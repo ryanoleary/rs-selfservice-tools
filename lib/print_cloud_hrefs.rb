6f2b61e5c5a2ff40d40355d0117d9c98a2f33c67
Ryan O'Leary <ryan.oleary@rightscale.com>
2014-04-27 19:51:06 +0000

    Fix indentation

--- lib/print_cloud_hrefs.rb
+++ lib/print_cloud_hrefs.rb
@@ -1,66 +1,66 @@
 def print_cloud_details( c )
 
-	c.clouds.index.each do |cloud|
-		
-		puts "=== CLOUD: " + cloud.name
-		puts cloud.name + " - " + cloud.href
+  c.clouds.index.each do |cloud|
+    
+    puts "=== CLOUD: " + cloud.name
+    puts cloud.name + " - " + cloud.href
 
-		if !cloud.raw["links"].detect{ |l| l["rel"] == "datacenters"}.nil?
+    if !cloud.raw["links"].detect{ |l| l["rel"] == "datacenters"}.nil?
 
-			puts "  == DATACENTERS"
-			cloud.show.datacenters.index.each do |d|
-				puts "    " + d.name + " - " + d.href
-			end
-		end
+      puts "  == DATACENTERS"
+      cloud.show.datacenters.index.each do |d|
+        puts "    " + d.name + " - " + d.href
+      end
+    end
 
-		if !cloud.raw["links"].detect{ |l| l["rel"] == "subnets"}.nil?
+    if !cloud.raw["links"].detect{ |l| l["rel"] == "subnets"}.nil?
 
-			puts "  == SUBNETS"
-			cloud.show.subnets.index.each do |d|
-				# Sometimes subnets names are nil, shouw the resource_uid instead
-				if d.name.nil?
-					sname = d.resource_uid
-				else
-					sname = d.name
-				end
-				puts "    " + sname + " - " + d.href
-			end
-		end
+      puts "  == SUBNETS"
+      cloud.show.subnets.index.each do |d|
+        # Sometimes subnets names are nil, shouw the resource_uid instead
+        if d.name.nil?
+          sname = d.resource_uid
+        else
+          sname = d.name
+        end
+        puts "    " + sname + " - " + d.href
+      end
+    end
 
-		if !cloud.raw["links"].detect{ |l| l["rel"] == "instance_types"}.nil?
+    if !cloud.raw["links"].detect{ |l| l["rel"] == "instance_types"}.nil?
 
-			puts "  == INSTANCE_TYPES"
-			cloud.show.instance_types.index.each do |d|
-				puts "    " + d.name + " - " + d.href
-			end
-		end
+      puts "  == INSTANCE_TYPES"
+      cloud.show.instance_types.index.each do |d|
+        puts "    " + d.name + " - " + d.href
+      end
+    end
 
-		if !cloud.raw["links"].detect{ |l| l["rel"] == "ssh_keys"}.nil?
+    if !cloud.raw["links"].detect{ |l| l["rel"] == "ssh_keys"}.nil?
 
-			puts "  == SSH_KEYS"
-			cloud.show.ssh_keys.index.each do |d|
-				puts "    " + d.resource_uid + " - " + d.href
-			end
-		end
+      puts "  == SSH_KEYS"
+      cloud.show.ssh_keys.index.each do |d|
+        puts "    " + d.resource_uid + " - " + d.href
+      end
+    end
 
-		if !cloud.raw["links"].detect{ |l| l["rel"] == "security_groups"}.nil?
+    if !cloud.raw["links"].detect{ |l| l["rel"] == "security_groups"}.nil?
 
-			puts "  == SECURITY_GROUPS"
-			cloud.show.security_groups.index.each do |d|
-				puts "    " + d.name + " - " + d.href
-			end
-		end
+      puts "  == SECURITY_GROUPS"
+      cloud.show.security_groups.index.each do |d|
+        puts "    " + d.name + " - " + d.href
+      end
+    end
 
-		# if !cloud.raw["links"].detect{ |l| l["rel"] == "images"}.nil?
+    # if !cloud.raw["links"].detect{ |l| l["rel"] == "images"}.nil?
 
-		# 	puts "== IMAGES"
-		# 	cloud.show.images.index.each do |d|
-		# 		puts d.name + " - " + d.href
-		# 	end
-		# end
+    #   puts "== IMAGES"
+    #   cloud.show.images.index.each do |d|
+    #     puts d.name + " - " + d.href
+    #   end
+    # end
 
-		puts ""
+    puts ""
 
-	end
+  end
 end
 
